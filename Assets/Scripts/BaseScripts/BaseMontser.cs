49de768639ee19b01e1e72c2dd01a330e1099828
mitomnguyen <ncd.ccpn@gmail.com>
2019-11-23 22:05:34 +0700

    update

---
+++ Assets/Scripts/BaseScripts/BaseMontser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseMonster
+{
+
+    public int ID;
+    public string Name;
+    public int TypeMonster;
+    public long BaseHp;
+    public long BaseDef;
+    public long BaseSpeed;
+    public int BaseGold;
+
+   
+    public BaseMonster SetBaseMonserFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<BaseMonster>(jsonString);
+    }
+}
---
+++ Assets/Scripts/TowerDefense/MainGameController.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+public class MainGameController : MonoBehaviour
+{
+    List<BaseMonster> BaseListMonster;
+
+    void Start()
+    {
+        string jsonBaseMonster = File.ReadAllText(Application.dataPath + "/BaseMonster.json");
+        BaseListMonster = new List<BaseMonster>();
+        Debug.Log(JArray.Parse(jsonBaseMonster));
+
+        SetBaseBaseListMonserFromJSON(JArray.Parse(jsonBaseMonster));
+
+        Debug.Log(BaseListMonster.Count);
+    }
+
+    void SetBaseBaseListMonserFromJSON(JArray jA)
+    {
+        for (int i = 0; i < jA.Count; i++)
+        {
+            string jsonString = ((JObject)jA[i]).ToString();
+            BaseMonster newMonster = new BaseMonster();
+            newMonster = newMonster.SetBaseMonserFromJSON(jsonString);
+            Debug.Log(jsonString);
+            BaseListMonster.Add(newMonster);
+        }
+    }
+}
