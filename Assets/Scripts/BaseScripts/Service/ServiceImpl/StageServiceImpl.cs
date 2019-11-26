313c65697b3ddecc4453e9e60f404bb396860768
mitomnguyen <ncd.ccpn@gmail.com>
2019-11-26 10:50:22 +0700

    update

---
+++ Assets/Scripts/BaseScripts/Service/ServiceImpl/StageServiceImpl.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+
+public class StageServiceImpl : StageService
+{
+
+    StageRepository stageRepository;
+
+    public StageServiceImpl()
+    {
+        stageRepository = new StageRepositoryImpl();
+    }
+
+    public List<Stage> findAll(MySqlCommand cmd)
+    {
+        return stageRepository.findAll(cmd);
+    }
+}
